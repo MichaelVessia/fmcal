488da832f19594eafc50b3c987c9111494711c67
Michael Vessia <michael@vessia.net>
2025-12-28 09:54:17 -0500

    feat: add nix package for NixOS installation

--- flake.nix
+++ flake.nix
@@ -4,21 +4,61 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    bun2nix = {
+      url = "github:nix-community/bun2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    bun2nix,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      bun2nix' = bun2nix.packages.${system}.default;
     in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "fmcal";
+        version = "0.1.0";
+        src = ./.;
+
+        nativeBuildInputs = [
+          bun2nix'.hook
+          pkgs.makeBinaryWrapper
+        ];
+
+        bunDeps = bun2nix'.fetchBunDeps {
+          bunNix = ./bun.nix;
+        };
+
+        # Skip default bun build - we'll run with bun interpreter
+        dontUseBunBuild = true;
+        dontUseBunCheck = true;
+        dontUseBunInstall = true;
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/lib/fmcal
+          cp -r . $out/lib/fmcal
+
+          mkdir -p $out/bin
+          makeBinaryWrapper ${pkgs.bun}/bin/bun $out/bin/fmcal \
+            --add-flags "run $out/lib/fmcal/src/main.ts"
+
+          runHook postInstall
+        '';
+      };
+
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           bun
           lefthook
           typescript
+          bun2nix'
         ];
 
         shellHook = ''
@@ -27,6 +67,7 @@
           echo "  bun install    - Install dependencies"
           echo "  bun run dev    - Run CLI in dev mode"
           echo "  bun run build  - Build executable"
+          echo "  bun2nix        - Generate bun.nix for packaging"
         '';
       };
     });
