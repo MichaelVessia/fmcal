95a71e5cabcc9363a8a4193bc0e8e118a1c2d569
Michael Vessia <michael@vessia.net>
2025-12-28 10:04:36 -0500

    Show help hint when running fmcal with no arguments

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { Command } from "@effect/cli"
-import { BunContext, BunRuntime } from "@effect/platform-bun"
-import { Effect, Layer } from "effect"
+import { Command } from "@effect/cli";
+import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { Console, Effect, Layer } from "effect";
 
-import { calendarsCommand } from "./commands/calendars.ts"
-import { createCommand } from "./commands/create.ts"
-import { deleteCommand } from "./commands/delete.ts"
-import { eventCommand } from "./commands/event.ts"
-import { eventsCommand } from "./commands/events.ts"
-import { freebusyCommand } from "./commands/freebusy.ts"
-import { updateCommand } from "./commands/update.ts"
-import { FastmailConfigLive } from "./config.ts"
-import { CalDavClientLive } from "./services/CalDavClient.ts"
+import { calendarsCommand } from "./commands/calendars.ts";
+import { createCommand } from "./commands/create.ts";
+import { deleteCommand } from "./commands/delete.ts";
+import { eventCommand } from "./commands/event.ts";
+import { eventsCommand } from "./commands/events.ts";
+import { freebusyCommand } from "./commands/freebusy.ts";
+import { updateCommand } from "./commands/update.ts";
+import { FastmailConfigLive } from "./config.ts";
+import { CalDavClientLive } from "./services/CalDavClient.ts";
 
-// Root command
+// Root command - show help hint when no subcommand provided
 const fmcal = Command.make("fmcal", {}, () =>
-  Effect.void
+  Console.log("Run 'fmcal --help' for usage information."),
 ).pipe(
   Command.withSubcommands([
     calendarsCommand,
@@ -24,23 +24,20 @@ const fmcal = Command.make("fmcal", {}, () =>
     updateCommand,
     deleteCommand,
     freebusyCommand,
-  ])
-)
+  ]),
+);
 
 // Compose layers
 const MainLayer = Layer.mergeAll(
   CalDavClientLive.pipe(Layer.provide(FastmailConfigLive)),
-  BunContext.layer
-)
+  BunContext.layer,
+);
 
 // CLI runner
 const cli = Command.run(fmcal, {
   name: "fmcal",
   version: "0.1.0",
-})
+});
 
 // Run
-Effect.suspend(() => cli(process.argv)).pipe(
-  Effect.provide(MainLayer),
-  BunRuntime.runMain
-)
+Effect.suspend(() => cli(process.argv)).pipe(Effect.provide(MainLayer), BunRuntime.runMain);
